4244c1be3380becf22b2676707ad4fd80d6a24f3
syth0le <chdan565@gmail.com>
2021-11-04 15:32:00 +0300

    products app starting

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,24 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    title = models.CharField()
+    slug = models.SlugField()
+    # images
+    brand = models.CharField()
+    description = models.TextField()
+    price = models.FloatField()
+    sale = models.FloatField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    category = models.ForeignKey(default='')
+
+
+class Categories(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    subcategory = models.CharField()
+
+
+class SubCategory(models.CharField):
+    pass
